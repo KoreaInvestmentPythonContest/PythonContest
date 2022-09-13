663c42d7f1d22aec17f0e6e09cef1cef72907007
jjiijj78 <jjiijj78@naver.com>
2022-09-13 21:27:43 +0900

    second test

--- main.py
+++ main.py
@@ -18,33 +18,78 @@ def get_html_one_class_find(data, tag, name):
 
 
 
-def for_naver_finance_new_article(url):
+def for_naver_finance_news_article(url):
     soup = parser(get_html(url))
-    new_area_text = soup.find("div", "news_area").find_all("a", recursive=True)
-    new_title_link =dict()
-    for idx, one_news in enumerate(new_area_text):
+    a_tag_list = soup.find("div", "news_area").find_all("a", recursive=True)
+    news_list = list()
+
+    for idx, one_news in enumerate(a_tag_list):
         news = re.findall('href="(.+?)">(.+?)</a>', str(one_news))[0]
         link = news[0].replace("amp;", "")  # 네이버 링크에서 amp;라는 것 때문에 접근이 불가능하여 별도 처리
-        new_title_link[news[1]] = url+link[:link.find(r'"')]
+        print("link",link)
+        news_list.append({ "title" : news[1],
+                            "link" : url+link[:link.find(r'"')]
+                         })
+
+    # print(news_list)
+    for idx, one_news in enumerate(news_list):
+        # if idx == 0:
+        article_link = one_news["link"]
+        soup_1 = parser(get_html(article_link))
+        main_article_html = soup_1.find("div",id="content")
+        # print(idx,one_news,main_article_html)
+        # main_article_html.find('div',class_='link_news').decompose()
+        # main_article = main_article_html.text
+        # news_list[idx]["main_text"] = main_article
 
 
-    for idx, tmp in enumerate(new_title_link):
-        print(idx,tmp,new_title_link[tmp])
-        if idx == 0:
-            article_link = new_title_link[tmp]
+
+
+    return news_list
+
+def for_naver_finance_news_article_add_more(url):
+    soup = parser(get_html(url))
+    new_area_text = soup.find("ul", class_="newsList").find_all("li", recursive=False)
+    news_list = list()
+
+
+    for idx, one_news in enumerate(new_area_text):
+        # if idx == 0:
+        a_tags =one_news.find_all("a", recursive=True)
+        # print(f"a_tags: {a_tags} ")
+        news = re.findall('href="(.+?)">(.+?)</a>', str(a_tags))[0]
+        # print(f"news: {news}")
+        link = news[0].replace("amp;", "")  # 네이버 링크에서 amp;라는 것 때문에 접근이 불가능하여 별도 처리
+        # print(f"link: {link}")
+        news_list.append({"title": news[1],
+                          "link": url[:url.find('/news/mainnews.naver')] + link
+                          })
+
+    # print(f"new_list : {news_list}")
+    for idx, one_news in enumerate(news_list):
+        # if idx == 0:
+            article_link = one_news["link"]
             soup_1 = parser(get_html(article_link))
-            # main_article = soup_1.find_all("div",id="content")
-            main_article = soup_1.find("div", {'id': 'content'}).text
-            print(main_article)ee
+            main_article_html = soup_1.find("div",id="content")
+            # print(idx,one_news,main_article_html)
+            main_article_html.find('div',class_='link_news').decompose()
+            main_article = main_article_html.text
+            news_list[idx]["main_text"] = main_article
+            print(news_list[idx])
 
-
-
-    return new_title_link
+    return news_list
 
 if __name__ == '__main__':
-    url_list = {'네이버금융': 'https://finance.naver.com', '네이버뉴스':'https://news.naver.com'}
-    url = url_list['네이버금융']
-    temps =for_naver_finance_new_article(url)
+    url_list = {'네이버금융': 'https://finance.naver.com',
+                '네이버뉴스':'https://news.naver.com',
+                '네이버금융_주요뉴스' : 'https://finance.naver.com/news/mainnews.naver'
+
+                }
+    # url = url_list['네이버금융']
+    # temps =for_naver_finance_news_article(url)
+
+    url = url_list['네이버금융_주요뉴스']
+    temps = for_naver_finance_news_article_add_more(url)
 
 
 
