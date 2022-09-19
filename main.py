ce8d3fcb5d3799745810809c524f8fd62a37f875
jjiijj78 <jjiijj78@naver.com>
2022-09-19 22:45:41 +0900

    Insert into NEWS_table to Mysql.db

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import re
 from config.UrlList import *
 from config.Market import *
@@ -19,11 +20,21 @@ class KoreaInvestment():
         self.Dcon =self.ConnectMySqlForDataFrame(self.MySqlGuest["host"],self.MySqlGuest["user"],self.MySqlGuest["password"])
 
     def function_start(self):
-        self.for_naver_finance_news_article_add_more(self.Url_list["네이버금융_주요뉴스"])
+        try:
+            with self.cur:
+                news_list = self.for_naver_finance_news_article_add_more(self.Url_list["네이버금융_주요뉴스"])
+                for one_news_dict in news_list:
+                    self.Insert("myDB.NEWS", one_news_dict)
+
+        # DB 연결 종료
+        finally:
+            self.con.close()
+
 
 
     def function_wiat(self):
-        self.for_naver_finance_news_article_add_more(self.Url_list["네이버금융_주요뉴스"])
+        self.Insert()
+
         self.select("STOCKS")
         self.get_code_list_by_market("KOSDAQ")  # 코스닥, 장내  구할려고
 
@@ -82,16 +93,17 @@ class KoreaInvestment():
                      'ListingDate': '19990811', 'SettleMonth': '12', 'Representative': '채형석, 이석주(각자 대표이사)',
                      'HomePage': 'http://www.aekyunggroup.co.kr'}
 
-        insert_data = "INSERT INTO  STOCKS( Market, Symbol, Name, Sector, Industry, ListingDate, SettleMonth, Representative, HomePage)" \
-                      f"VALUES ({test_dict['Market']},{test_dict['Symbol']},{test_dict['Name']},{test_dict['Sector']},{test_dict['Industry']}," \
-                      f"{test_dict['ListingDate']},{test_dict['SettleMonth']},{test_dict['Representative']},{test_dict['HomePage']})"
 
-        temp_str = "INSERT INTO STOCKS("
-        for key, value in test_dict.items():
+
+        temp_str = f"INSERT INTO {table_name}("
+        for key, value in dict.items():
             temp_str = temp_str + key + ','
         temp_str = temp_str[:-1] + ") VALUES ("
 
-        for key, value in test_dict.items():
+        for key, value in dict.items():
+            #value에 ', " 변경
+            if type(value) == str:
+                value =value.replace("\'" , "\\\'")
             temp_str = temp_str + f"'{value}',"
         temp_str = temp_str[:-1] + ");"
         print(temp_str)
@@ -102,12 +114,36 @@ class KoreaInvestment():
         except pymysql.Error as err:
             print("Something went wrong: {}".format(err))
             self.con.rollback()
-        # 데이타 Fetch
-        # for one_fetch in cur.fetchall():
-        #     print(one_fetch)  # 전체 rows
 
-        # STEP 5: DB 연결 종료
-        self.con.close()
+        # DB 연결 종료
+        # finally:
+        #     self.con.close()
+
+    def InsertFromSelect(self, table_name, dict):
+        # key :col_name, value : data
+        test_dict = {'Market': 'KOSPI', 'Symbol': '006840', 'Name': 'AK홀딩스', 'Sector': '기타 금융업', 'Industry': '지주사업',
+                     'ListingDate': '19990811', 'SettleMonth': '12', 'Representative': '채형석, 이석주(각자 대표이사)',
+                     'HomePage': 'http://www.aekyunggroup.co.kr'}
+
+        temp_str = f"INSERT INTO {table_name}("
+        for key, value in dict.items():
+            temp_str = temp_str + key + ','
+        temp_str = temp_str[:-1] + ") SELECT "
+
+        for key, value in dict.items():
+            temp_str = temp_str + f"'{value}',"
+        temp_str = temp_str[:-1] + f" FROM {table_name} "
+        print(temp_str)
+
+        # try:
+        #     self.cur.execute(temp_str)
+        #     self.con.commit()
+        # except pymysql.Error as err:
+        #     print("Something went wrong: {}".format(err))
+        #     self.con.rollback()
+
+        # DB 연결 종료
+        # self.con.close()
 
     def select(self,TableName='',Market='',Symbol=''):
         temp_str = f"SELECT * FROM {TableName} A WHERE ( '{Market}' = '' or A.Market = '{Market}')and ('{Symbol}'= '' or A.Symbol ='{Symbol}');"
@@ -173,28 +209,40 @@ class KoreaInvestment():
 
 
         for idx, one_news in enumerate(new_area_text):
-            # if idx == 0:
+            # if idx == 2:
             a_tags =one_news.find_all("a", recursive=True)
-            # print(f"a_tags: {a_tags} ")
             news = re.findall('href="(.+?)">(.+?)</a>', str(a_tags))[-1]
             # print(f"news: {news}")
             link = news[0].replace("amp;", "")  # 네이버 링크에서 amp;라는 것 때문에 접근이 불가능하여 별도 처리
             # print(f"link: {link}")
-            news_list.append({"title": news[1],
-                              "link": url[:url.find('/news/mainnews.naver')] + link
+            news_list.append({"OCCR_DT": datetime.today().strftime("%Y%m%d"),
+                              "TITLE": news[1],
+                              "OCCR_LOC" : "네이버금융_주요뉴스",
+                              "URL": url[:url.find('/news/mainnews.naver')] + link
                               })
 
         # print(f"new_list : {news_list}")
         for idx, one_news in enumerate(news_list):
+            img_url_list = list()
             # if idx == 0:
-                article_link = one_news["link"]
-                soup_1 = self.parser(self.get_html(article_link))
-                main_article_html = soup_1.find("div",id="content")
-                # print(idx,one_news,main_article_html)
-                main_article_html.find('div',class_='link_news').decompose()
-                main_article = main_article_html.text.replace("\n","").replace("\t","")
-                news_list[idx]["main_text"] = main_article
-                print(news_list[idx])
+            article_link = one_news["URL"]
+            soup_1 = self.parser(self.get_html(article_link))
+            main_article_html = soup_1.find("div",id="content")
+            img_links =main_article_html.find_all("img", recursive=True)
+
+            #이미지가 없을경우
+            if len(img_links) != 0:
+
+                for idx2, img_link in enumerate(img_links):
+                    img_url_list.append(img_link['src'])
+
+            main_article_html.find('div',class_='link_news').decompose() #뒤에 잡다한 데이터가 들어와서
+
+            # main_article = main_article_html.text.replace("\n","").replace("\t","")
+            main_article = main_article_html.text.replace("\t", "")
+            news_list[idx]["TEXT"] = main_article.strip()
+            news_list[idx]["IMG_URL_LIST"] = ''.join(img_url_list)
+            # print(news_list[idx])
 
         return news_list
 
---
+++ testfile.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+# YYYYmmddHHMMSS 형태의 시간 출력
+print(datetime.today().strftime("%Y%m%d"))
