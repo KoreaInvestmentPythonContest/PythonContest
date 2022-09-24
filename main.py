9447117c1b2a02cb4fe9b0c8147892ece7c60784
jjiijj78 <jjiijj78@naver.com>
2022-09-24 21:11:36 +0900

    Update NEWS table for extr_stck_cd_list

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ class KoreaInvestment():
     def function_start(self):
         self.get_stocks_from_news()
 
-
-
     def function_wiat(self):
         self.Insert()
 
@@ -34,7 +32,7 @@ class KoreaInvestment():
             with self.cur:
                 news_list = self.for_naver_finance_news_article_add_more(self.Url_list["네이버금융_주요뉴스"])
                 for one_news_dict in news_list:
-                    self.Insert("myDB.NEWS", one_news_dict)
+                    self.Insert(table_name="myDB.NEWS",dict=one_news_dict)
 
         # DB 연결 종료
         finally:
@@ -43,23 +41,36 @@ class KoreaInvestment():
     def get_stocks_from_news(self):
         base_dict = dict()
         base_dict["ANAL_YN"] = 'N'
+        news_tuple =self.select(TableName="myDB.NEWS",selectList=["SEQ","OCCR_DT","TITLE","TEXT"],where_dict=base_dict, orderbyacsList=["SEQ","OCCR_DT"])
+        base_dict =dict()
+        stocks_tuple = self.select(TableName="myDB.STOCKS",selectList=["Market","Symbol","Name"], orderbyacsList=["Market","Symbol","Name"])
 
-        news_tuple =self.select(TableName="myDB.NEWS", where_dict=base_dict, orderbyacslist =["SEQ"])
-        ''' 
-            SEQ          0
-            OCCR_DT      1      
-            OCCR_LOC     2
-            TITLE        3
-            URL          4
-            TEXT         5
-            ANAL_YN      6
-            EXTR_YN      7
-            EXTR_STCK_NM 8
-            IMG_URL_LIST 9
-        '''
+        #NEWS순서
+        SEQ     = 0
+        OCCR_DT = 1
+        TITLE   = 2
+        TEXT    = 3
 
+        #STOCKS
+        Market = 0
+        Symbol = 1
+        Name   = 2
 
-
+        test_list = list()
+        for new_idx, one_news_tuple in enumerate(news_tuple):
+            test_dict = dict()
+            test_dict["SEQ"] = one_news_tuple[SEQ]
+            test_dict["Symbol"] = list()
+            for stock_idx, one_stock_tuple in enumerate(stocks_tuple):
+                if one_news_tuple[TEXT].find(one_stock_tuple[Name]) != -1:#발견시
+                    test_dict["Symbol"].append(one_stock_tuple[Symbol])
+            test_list.append(test_dict)
+        for idx, one_dict  in enumerate(test_list):
+            if len(one_dict['Symbol']) == 0: #추출된 종목이 없을때
+                self.update(TableName="myDB.NEWS",setDict={"ANAL_YN":"Y", "EXTR_YN":"N"},where_dict={"SEQ":one_dict['SEQ']})
+            else:
+                self.update(TableName="myDB.NEWS", setDict={"ANAL_YN":"Y","EXTR_YN":"Y","EXTR_STCK_CD_LIST": one_dict['Symbol']},
+                            where_dict={"SEQ": one_dict['SEQ']})
 
     def get_code_list_by_market(self, market_code):
         '''
@@ -111,7 +122,7 @@ class KoreaInvestment():
         conn = db_connection.connect()
         return conn
 
-    def Insert(self,table_name,dict):
+    def Insert(self,table_name='',dict=dict()):
         #key :col_name, value : data
         test_dict = {'Market': 'KOSPI', 'Symbol': '006840', 'Name': 'AK홀딩스', 'Sector': '기타 금융업', 'Industry': '지주사업',
                      'ListingDate': '19990811', 'SettleMonth': '12', 'Representative': '채형석, 이석주(각자 대표이사)',
@@ -169,10 +180,19 @@ class KoreaInvestment():
         # DB 연결 종료
         # self.con.close()
 
-    def select(self,TableName='',where_dict =dict(),orderbyacslist =list()):
+    def select(self,TableName='', selectList=list(), where_dict=dict(), orderbyacsList=list()):
 
         # temp_str = f"SELECT * FROM {TableName} A WHERE ( '{Market}' = '' or A.Market = '{Market}')and ('{Symbol}'= '' or A.Symbol ='{Symbol}');"
         temp_str = f"SELECT * FROM {TableName} A "
+
+        #select 절 추가하기
+        if len(selectList) == 0:
+            temp_str = f"SELECT * FROM {TableName} A "
+        else:
+            temp_str = f"SELECT  "
+            for one_select in selectList:
+                temp_str += f"{one_select} ,"
+            temp_str = temp_str[:-1] +f" FROM {TableName} A "
 
         # where절 추가하기 key,value값없으면 그냥 조회한다
         if len(where_dict) != 0 :
@@ -184,9 +204,9 @@ class KoreaInvestment():
                     temp_str += f"AND A.{key} = {value}"
 
         # order by 추가하기
-        if len(orderbyacslist) != 0:
+        if len(orderbyacsList) != 0:
             temp_str += "order by "
-            for one_value in orderbyacslist:
+            for one_value in orderbyacsList:
                 temp_str += f"{one_value} ,"
             temp_str = temp_str[:-1]
 
@@ -201,6 +221,39 @@ class KoreaInvestment():
         except pymysql.Error as err:
             print("Something went wrong: {}".format(err))
 
+    def update(self,TableName='', setDict=dict(), where_dict=dict()):
+
+        temp_str = f"UPDATE {TableName} A SET "
+
+        # SET 절 추가하기
+        for key,value in setDict.items():
+            if type(value) == str:
+                temp_str += f"A.{key} = \'{value}\' ,"
+            elif type(value) == list:
+                temp_str += f"A.{key} = \'{','.join(value)}\' ,"
+            else:
+                temp_str += f"A.{key} = {value} ,"
+        temp_str = temp_str[:-1]
+
+        # where절 추가하기 key,value값없으면 그냥 조회한다
+        if len(where_dict) != 0:
+            temp_str += "WHERE 1=1 "
+            for key,value in where_dict.items():
+                if type(value) == str:
+                    temp_str += f"AND A.{key} = \'{value}\'"
+                elif type(value) == list:
+                    temp_str += f"AND A.{key} = \'{','.join(value)}\'"
+                else:
+                    temp_str += f"AND A.{key} = {value}"
+
+
+        print(temp_str)
+        try:
+            self.cur.execute(temp_str)
+            self.con.commit()
+        except pymysql.Error as err:
+            print("Something went wrong: {}".format(err))
+            self.con.rollback()
 
     def get_html(self,url):
         headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
