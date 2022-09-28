bd247643e4ab2bfeff69cf7b88518c2e002b57f4
jjiijj78 <jjiijj78@naver.com>
2022-09-28 21:35:35 +0900

    for server starting

--- __init__.py
+++ __init__.py
@@ -2,7 +2,8 @@ from main import *
 
 import sys
 
-class Ui_class():
+
+class Ui_class:
     def __init__(self):
         print("Ui_class 입니다")
         self.KoreaInvestment = KoreaInvestment()
@@ -10,4 +11,4 @@ class Ui_class():
 
 
 if __name__ == "__main__":
-    ui_class =Ui_class()
+    ui_class = Ui_class()
